cbf8273526cab580baf8b5e1eaf8697f850da9dc
Toni Tegar Sahidi <tonitegarsahidi@gmail.com>
2021-05-10 15:18:19 +0700

    finish the MVP and readme. ready to be used

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
 # NinjaProtect
-protect website from copy and paste, right click, inspect element, etc.
+
+NinjaProtect is a class library to **protect your website from copying or modifying activities such as right click, copy, and modify content**. 
+
+NinjaProtect applies in front-end as 'basic' defense from 'novice' / typical user who wants to copy content or image from your website. 
+
+Here are the activities list which Ninja can protect : 
+#### Protected Mouse Activities
+* (Mouse) Disable **Right Click**
+* (Mouse) Disable **Select Text & Image**
+* (Mouse) Disable **Drag an element** _(e.g. drag images to new tab)_
+#### Protected Keyboard Activities
+* (Shortcut) Disable Ctrl-A (Select All)
+* (Shortcut) Disable Ctrl-C (Copy)
+* (Shortcut) Disable Ctrl-X (Cut)
+* (Shortcut) Disable Ctrl-U (View Source)
+* (Shortcut) Disable Ctrl-P (Print)
+* (Shortcut) Disable Ctrl-S (Save As Page)
+* (Shortcut) Disable Ctrl-Shift-i (Inspect Element)
+* (Shortcut) Disable F12 (Developer Mode)
+
+
+
+## Installation
+
+1. Clone or Download this repository
+2. Copy ninjaprotect.js to your JS folder, 
+3. Include it in your HTML file.
+```html
+<script src="ninjaprotect.js" >
+```
+
+## Usage
+
+#### Standard Protection
+This is the default protection, as it cover alls protection mentioned above. Create the object then call the ```protectAll()``` function. 
+
+```html
+<script>
+
+var guardian = new NinjaProtect();
+guardian.protectAll();
+
+</script>
+```
+
+#### Custom Protection
+You can specifiy what to protect, by passing configuration when creating the object. then calls ```protect()``` function.
+```html
+<script>
+
+        var myconfig = {
+            'disableRightClick' : false, 
+            'disableSelect' : false, 
+            'disableDrag' : false,   
+          
+            'disableCopy' : true,   
+            'disableCut' : false, 
+            'disablePrint' : false, 
+            'disableSave' : false, 
+            'disableSelectAll' : false, 
+    
+            'disableViewSource' : false,     
+            'disableDeveloperMode' : false, 
+            'disableInspectElement' : false, 
+            }
+
+        var guardian = new NinjaProtect(myconfig);
+        guardian.protect();
+
+</script>
+```
+
+## Disclaimer
+1. There is no 100%  protection. As the creator, I know that there's still way to view the code / source code from the web.
+2. Advance user who know about web development, still able to 'copy' your content through various ways.
+3. This script is not yet tested in various browser environment
+
+
+## Contributing
+* Pull requests are welcome. 
+* For major changes, please open an issue first to discuss what you would like to change.
+
+## To Do Next
+1. Protect from ```PrintScreen```
+2. NPM package
+3. WP Plugin package
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- ninjaprotect.js
+++ ninjaprotect.js
@@ -1,50 +1,116 @@
-var ninjaprotect = {};
+
 
 class NinjaProtect{
 
     config = {};
 
-    constructor(config = {
-        'disableRightClick' : true, 
-        'disableSelect' : true, 
-        'disablePageSource' : true, 
-        'disableDeveloperMode' : true, 
-        'disableInspectElement' : true, 
-        'disableCopy' : true,   
-        'disablePaste' : true, 
-        'disablePrint' : true, 
-        'disableSelectAll' : true,     
-        })
+    constructor(config = {})
     {
         this.config = config;
-
     }
-
+    //automatically protect for all
     protectAll(){
-        if(this.config.disableSelect) { this.disableSelectAll()};
-
-        if(this.config.disableCopy) {this.disableCopy()};
-
-        console.log("saya protecting ya");
-        console.log(this.config);
-
-    }
-
-
-
-    disableCopy(){
-        this.disableCombiCtrlKeys(67);
-        console.log("disabling copy");
-    }
-
-    disableSelectAll(){
-        this.disableCombiCtrlKeys(65);
-    }
-
-
+        this.config = {
+            'disableRightClick' : true, 
+            'disableSelect' : true, 
+            'disableDrag' : true, 
     
-    disableCombiCtrlKeys(keynumber){
-        window.addEventListener("keydown", function(e) {
+           
+            'disableDeveloperMode' : true, 
+            'disableInspectElement' : true, 
+            'disableCopy' : true,   
+            'disableCut' : true, 
+            'disablePrint' : true, 
+            'disableSave' : true, 
+    
+            'disableSelectAll' : true,     
+            'disableViewSource' : true,     
+            }
+        this.protect();
+
+    }
+
+    /**
+     * Apply Rules based on given config
+     */
+    protect(){
+
+        this.config.disableSelectAll        ? this.protectCombiCtrlKeys(65) : false;  //disable Ctrl A
+        this.config.disableCopy             ? this.protectCombiCtrlKeys(67) : false;   //disable Ctrl C
+        this.config.disableCut              ? this.protectCombiCtrlKeys(88) : false;   //disable Ctrl X
+        this.config.disableViewSource       ? this.protectCombiCtrlKeys(85) : false;   //disable Ctrl U / view source
+        this.config.disablePrint            ? this.protectCombiCtrlKeys(80) : false;   //disable Ctrl P / Print Page
+        this.config.disableSave             ? this.protectCombiCtrlKeys(83) : false;   //disable Ctrl S / Print Page
+        this.config.disableInspectElement   ? this.protectCombiCtrlShiftKeys(73) : false;   //disable Ctrl + Shift + i
+        this.config.disableDeveloperMode    ? this.protectKeys(123) : false;   //disable F12
+
+        this.config.disableRightClick       ? this.protectRightClick() : false;    //disable right clieck
+        this.config.disableSelect           ? this.protectSelectElement() : false; //disable select element / text
+        this.config.disableDrag             ? this.protectDragElement() : false;   //disable drag element
+
+    }
+
+
+
+    /**
+     * disable user from selecting an element
+     */
+    protectSelectElement(){
+        let e = document.body;
+        if (typeof e.onselectstart != "undefined") e.onselectstart = function() {
+            return false
+        };
+        else if (typeof e.style.MozUserSelect != "undefined") e.style.MozUserSelect = "none";
+        else e.onmousedown = function() {
+                return false
+            };
+        e.style.cursor = "default"
+
+    }
+
+    /**
+     * disable drag element, e.g. drag images to new tab
+     */
+    protectDragElement(){
+        document.ondragstart = function() {
+            return false
+        };
+    }
+
+    /**
+     * disable right Click, but enable only if it is hyperlink
+     */
+    protectRightClick(){
+        
+        document.oncontextmenu = function(e) {
+            //uncomment these lines if you want right click enabled for hyperlink. however, the 'inspect element' menu will also show
+            // var t = e || window.event;
+            // var n = t.target || t.srcElement;
+            // if (n.nodeName != "A") return false
+            return false;
+        };
+    }
+
+    protectKeys(keynumber){
+        return window.addEventListener("keydown", function(e) {
+
+            if (e.key !== undefined || e.which !== undefined) {
+
+                if (e.key == keynumber) {
+                    e.preventDefault()
+
+                   
+                }
+                else if (e.which == keynumber) {   //for older browser suport
+                    e.preventDefault()
+                }
+            }
+            
+        });
+    }
+
+    protectCombiCtrlKeys(keynumber){
+        return window.addEventListener("keydown", function(e) {
 
             if (e.key !== undefined || e.which !== undefined) {
 
@@ -57,17 +123,28 @@ class NinjaProtect{
                     e.preventDefault()
                 }
             }
-
-            console.log("saya dipanggil")
             
         });
+    }
 
+    protectCombiCtrlShiftKeys(keynumber){
+        return window.addEventListener("keydown", function(e) {
 
+            if (e.key !== undefined || e.which !== undefined) {
 
+                if (e.ctrlKey && e.shiftKey && e.key == keynumber) {
+                    e.preventDefault()
+
+                   
+                }
+                else if (e.ctrlKey  && e.shiftKey && e.which == keynumber) {   //for older browser suport
+                    e.preventDefault()
+                }
+            }
+            
+        });
     }
 
 
-
-
-}
+}   //end class
 
